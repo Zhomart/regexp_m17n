6f2bd321beef2b9f2c1c6a5f4bc39dc0acfa66e9
Zhomart Mukhamejanov <mzhomart@gmail.com>
2014-08-08 12:25:06 -0700

    ok

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,22 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    cool_encoding = undummified_encoding(str.encoding)
+
+    pattern = "^.+$".encode(cool_encoding)
+    str = str.encode(cool_encoding)
+
+    str =~ Regexp.new(pattern)
+  end
+
+  def self.undummified_encoding(encoding)
+    return encoding unless encoding.dummy?
+    case encoding.name
+    when 'UTF-16'
+      'UTF-16BE'
+    when 'UTF-32'
+      'UTF-32BE'
+    else
+      'UTF-8'
+    end
   end
 end
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -1,11 +1,16 @@
 # encoding: utf-8
 require 'minitest/autorun'
 require_relative '../lib/regexp_m17n'
+require 'iconv'
 
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
-    Encoding.list.each do |enc|
-      assert(RegexpM17N.non_empty?('.'.encode(enc)))
+    raw_str = "."
+
+    Encoding.list.each do |enc, index|
+      str = raw_str.encode(enc) rescue Iconv.conv(raw_str.encoding.name, enc.name, raw_str)
+
+      assert(RegexpM17N.non_empty?(str))
     end
   end
 end
